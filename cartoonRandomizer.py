84f63f22a677565d2e5fc7fb7ebbec2275f79645
Superchordate <superchordate@gmail.com>
2018-10-30 21:01:57 -0500

    my attempt

--- cartoonRandomizer.py
+++ cartoonRandomizer.py
@@ -1,83 +1,59 @@
 #!/usr/bin/python
 
-# import probably too much stuff
 import RPi.GPIO as GPIO
 import os
 import sys
 import time
 import random
-import subprocess 
+import subprocess
 
-# set variables that match the pins on the PI for the buttons
-simpsonsButton = 17
-futuramaButton = 27
-disenchantmentButton = 22
-
-# set variables for directories with video files
-simpsonsDirectory = "/home/pi/Videos/Simpsons/"
-futuramaDirectory = "/home/pi/Videos/Futurama/"
-disenchantmentDirectory = "/home/pi/Videos/Disenchantment/"
-
-# initiate variables for use in functions
 episode = None
 omxc = None
 previousEpisode = []
-
-# set up the GPIO stuff for the PI buttons
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(simpsonsButton, GPIO.IN)
-GPIO.setup(futuramaButton, GPIO.IN)
-GPIO.setup(disenchantmentButton, GPIO.IN)
-
-# functions for each show / button
-
-## works-ish but sloppy and old
-def playSimpsons():
-    episode = random.choice(os.listdir(simpsonsDirectory))
-    os.system('killall omxplayer.bin')
-    omxc = subprocess.Popen(['omxplayer', '-b', simpsonsDirectory + episode, ' &'])
-    time.sleep(.3)
-
-## working function to use as model for other functions
-def playFuturama():
-    global episode
-    global previousEpisode
-    global omxc
-
-    if episode != None:
-        previousEpisode.append(episode)
-
-    episode = random.choice(os.listdir(futuramaDirectory))
+    
+def playEp( directory ):
+    
+    global episode, omxc, previousEpisode
+    
+    if episode != None: previousEpisode.append(episode)
+    
+    episode = random.choice( os.listdir(directory) )
 
     while episode in previousEpisode:
-        episode = random.choice(os.listdir(futuramaDirectory))
-
+        episode = random.choice(os.listdir(directory))
+    
     if omxc != None:
-        subprocess.Popen(['killall', 'omxplayer.bin'])
-
-    omxc = subprocess.Popen(['omxplayer', '-b', futuramaDirectory + episode])
-    time.sleep(.3)
-
-## works-ish but sloppy and old
-def playDisenchantment():
-    episode = random.choice(os.listdir(disenchantmentDirectory))
-    os.system('killall omxplayer.bin')
-    omxc = subprocess.Popen(['omxplayer', '-b', disenchantmentDirectory + episode, ' &'])
-    time.sleep(.3)
-
-# main code to wait for button presses.  
-try: 
-    while True:
-        if GPIO.input(simpsonsButton) == 1:
-            playSimpsons()
+        subprocess.Popen( [ 'killall', 'omxplayer.bin' ] )
+    else:
+        os.system( 'killall omxplayer.bin' )
+        
+    omxc = subprocess.Popen( [ 'omxplayer', '-b', simpsonsDirectory + episode, ' &' ] )
     
-        if GPIO.input(futuramaButton) == 1:
-            playFuturama()
+def playSimpson():
+    playEp( "/home/pi/Videos/Simpsons/" )
     
-        if GPIO.input(disenchantmentButton) == 1:
-            playDisenchantment()
-
-# reset GPIO on ctrl-c
-except KeyboardInterrupt:
+def playFuturama():
+    playEp( "/home/pi/Videos/Futurama/" )
+    
+def playDisen():
+    playEp( "/home/pi/Videos/Disenchantment/" )
+    
+def cleanUp():
     GPIO.cleanup()
 
+# button setup and listeners.
+# from http://raspberry.io/projects/view/reading-and-writing-from-gpio-ports-from-python/
+
+GPIO.setmode(GPIO.BCM)
+
+def addCallback( buttonNum, callback ):
+    
+    GPIO.setup( buttonNum, GPIO.IN, pull_up_down = GPIO.PUD_DOWN )
+    GPIO.add_event_detect( buttonNum, GPIO.BOTH )
+    GPIO.add_event_callback( buttonNum, callback)
+
+addCallback( 17, playSimpson )
+addCallback( 27, playFuturama )
+addCallback( 22, playSimpson )
+
+# Also add callback for ctrl+c?
